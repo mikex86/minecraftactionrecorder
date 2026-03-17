2676754a4ccbceaea7cbd76b96423c3a3b5bdc9b
mike <michael.keiblinger@gmail.com>
2026-03-17 01:02:07 -0700

    fix bug

--- src/client/java/me/michael/kei/actionrecorder/ActionRecorder.java
+++ src/client/java/me/michael/kei/actionrecorder/ActionRecorder.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.telemetry.TelemetryProperty;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
