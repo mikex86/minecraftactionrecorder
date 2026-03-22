859a4a9e8ff9eb81316ee812e4c721b0266e8119
mike <michael.keiblinger@gmail.com>
2026-03-22 16:40:23 -0700

    push remaining files

---
+++ src/main/java/me/michael/kei/actionrecorder/MinecraftActionRecorderNeoForge.java
@@ -0,0 +1,11 @@
+package me.michael.kei.actionrecorder;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(MinecraftActionRecorderNeoForge.MOD_ID)
+public final class MinecraftActionRecorderNeoForge {
+    public static final String MOD_ID = "minecraftactionrecorder";
+
+    public MinecraftActionRecorderNeoForge() {
+    }
+}
